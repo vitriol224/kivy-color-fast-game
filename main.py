458b4b48dad64e5601f7a722c3a75f4670821538
vitriol224 <vitriolc@gmail.com>
2016-07-11 11:49:03 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from kivy.uix.widget import Widget
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager,Screen 
 from kivy.uix.screenmanager import FadeTransition
 from kivy.lang import Builder
@@ -268,6 +269,32 @@ class ScreenManagement(ScreenManager,Widget):
 
         
     ##############################################
+    
+    def confirm_quit(self):
+        self.box=FloatLayout()
+        
+        self.lab=(Label(text="Do you want to quit? ",font_size=30,
+        	size_hint=(None,None),pos_hint={'x':.25,'y':.6}))
+        self.box.add_widget(self.lab)
+        
+        self.but=(Button(text="No",size_hint=(None,None),
+        	width=self.width/3,height=70,pos_hint={'x':0,'y':0}))
+        self.box.add_widget(self.but)
+
+        self.but2=(Button(text="Yes",size_hint=(None,None),
+        	width=self.width/3,height=70,pos_hint={'right':1,'y':0}))
+        self.box.add_widget(self.but2)
+       
+        self.main_pop = Popup(title="QUIT GAME ?",content=self.box,
+        	size_hint=(.8,.5),auto_dismiss=False,title_size=25)
+        	
+        self.but.bind(on_press=self.main_pop.dismiss)
+        #self.but2.bind(on_press=self.quit_game)
+        
+        self.main_pop.open()
+
+    ##############################################
+        
 
     
     def quit_game(self):
